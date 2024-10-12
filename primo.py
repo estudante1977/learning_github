93a9e09961ad906f06f1a797a617effd5393c2a3
estudante1977 <antonio-jairo@hotmail.com>
2024-10-12 08:43:51 -0300

    Descobre se o número é primo.
    Este código escrito em python, consegue reconhecer se um número é primo ou não.

---
+++ primo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+number = input("Enter a number, and I'll tell you if it's prime: ")
+number = int(number)
+
+if is_prime(number):
+    print(f"\nThe number {number} is prime.")
+else:
+    print(f"\nThe number {number} is not prime.")
